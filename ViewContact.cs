3f03e6888f0f7b6dcc49146936d56a85343aae3d
İlknur Özdemir <43750397+ilknurrozdmr@users.noreply.github.com>
2022-04-06 01:25:12 +0300

    Add files via upload

---
+++ ViewContact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonRehberiUygulaması
+{
+    class ViewContact
+    {
+        public void kisiGoruntule(List<Contact> kisiler)
+        {
+            Console.WriteLine("Telefon Rehberi");
+            Console.WriteLine("*********************************");
+            foreach (var item in kisiler)
+            {
+                Console.WriteLine("İsim: " + item.FirstName);
+                Console.WriteLine("Soyisim: " + item.LastName);
+                Console.WriteLine("Telefon Numarası: " + item.Phone);
+            }
+        }
+    }
+}
