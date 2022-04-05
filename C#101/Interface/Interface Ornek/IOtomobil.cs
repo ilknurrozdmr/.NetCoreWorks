a550067c10200e8e11d1124cbaefe000c6498ac4
İlknur Özdemir <43750397+ilknurrozdmr@users.noreply.github.com>
2022-04-05 17:57:14 +0300

    Create IOtomobil.cs

---
+++ C#101/Interface/Interface Ornek/IOtomobil.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Ornek
+{
+    public interface IOtomobil
+    {
+        int KacTekerlektenOlusur();
+
+        Marka HangiMarkaninAraci();
+
+        Renk StandartRengiNe();
+    }
+}
